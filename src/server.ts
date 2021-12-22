972b82453bba7ff28b91a37d49a602c36f220aba
Yan Soares <yts.1995@gmail.com>
2021-12-21 23:14:08 -0300

    chore: improve middleware quality

--- src/server.ts
+++ src/server.ts
@@ -7,17 +7,15 @@ const app = express()
 app.use(json())
 app.use(routes)
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof Error) {
-    return res.status(400).json({
-      message: err.message
+app.use(
+  (err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) return res.status(400).json({ message: err.message })
+
+    return res.status(500).json({
+      status: "Error",
+      message: "Internal server error"
     })
   }
+)
 
-  return response.status(500).json({
-    status: "Error",
-    message: "Internal server error."
-  })
-})
-
-app.listen(3333, () => console.log('Server runnning at http://localhost:3333'))
+app.listen(3333, () => console.log('Server runnning!'))
