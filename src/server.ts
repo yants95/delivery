9ccc710e3290e4256035028bc8f7e9c6ddf0a201
Yan Soares <yts.1995@gmail.com>
2021-12-21 22:07:40 -0300

    chore: add express-async-errors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,23 @@
-import express, { json } from 'express'
+import express, { json, NextFunction, Request, response, Response } from 'express'
+import 'express-async-errors'
 import { routes } from './routes'
 
 const app = express()
 
 app.use(json())
 app.use(routes)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      message: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "Error",
+    message: "Internal server error."
+  })
+})
 
 app.listen(3333, () => console.log('Server runnning at http://localhost:3333'))
