fe498359fdae16c4d16385ac298e31b9bf7f0087
Yan Soares <yts.1995@gmail.com>
2021-12-23 11:17:47 -0300

    refactor: improve code quality

--- src/modules/deliveries/usecases/create-delivery/create-delivery-usecase.ts
+++ src/modules/deliveries/usecases/create-delivery/create-delivery-usecase.ts
@@ -1,5 +1,6 @@
 import { CreateDelivery } from '@/modules/deliveries/usecases/create-delivery/dtos'
 import { prisma } from '@/database'
+
 export class CreateDeliveryUsecase {
   async execute ({ item_name, client_id }: CreateDelivery): Promise<void> {
     await prisma.deliveries.create({
